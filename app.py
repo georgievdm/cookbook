77df0cdedb3f5a73883982127226503b71ade1b9
jeanette <jeanette@vandermerwe.co.za>
2018-08-09 21:07:09 +0100

    Add read me file with initial notes

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+
+app= Flask(__name__)
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = 'mongodb://admin:admin123@ds217092.mlab.com:17092/task_manager'
+
+mongo = PyMongo(app)
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Task Manager'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find())
+
+
+#@app.route('/')
+#def hello():
+#    return 'Task Manager'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
